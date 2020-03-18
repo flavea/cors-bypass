fafe1ac72d3f587412f7ac52e0ec54384063721d
Ilma Arifiany <iarifiany@gmail.com>
2020-03-18 18:29:38 +0700

    Make fetch options a payload for more flexibility

--- src/routes/proxy.js
+++ src/routes/proxy.js
@@ -3,15 +3,7 @@ const rq = require('request-promise')
 module.exports = {
     proxyHandler: async (request) => {
         if (request.payload.token && request.payload.token === process.env.token) {
-            var options = {
-                'method': 'POST',
-                'url': request.query.url,
-                'headers': {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(request.payload),
-
-            }
+            const options = request.payload
 
             try {
                 const fetch = await rq(options)
